c61242e0f5f22f8afec632675cf64bf7a4423ca4
PedroViniciusMD <pedrodelgado583@gmail.com>
2026-05-08 20:32:41 -0300

    fix: mudança logica AppBar

--- lib/view/widgets/clube_appbar_widget.dart
+++ lib/view/widgets/clube_appbar_widget.dart
@@ -2,12 +2,20 @@ import 'package:flutter/material.dart';
 
 class ClubeAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
+  final Color corDoTexto;
+  final bool iconeMenu;
+  final bool iconeCarrinho;
+  final bool iconeSeta;
 
   const ClubeAppBar({
     super.key,
     required this.title,
+    required this.iconeMenu,
+    required this.iconeCarrinho,
+    required this.iconeSeta,
+    required this.corDoTexto,
   });
-
+  //TODO: criar um widget para o Drawer
   //TODO: config alutra e largura appbar - OK
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight + 1.0);
@@ -18,42 +26,49 @@ class ClubeAppBar extends StatelessWidget implements PreferredSizeWidget {
       backgroundColor: const Color(0xFFF5F1EB),
       elevation: 0,
       centerTitle: false,
-      titleSpacing: 20,
-      leading: Builder(
-        builder: (context) => IconButton(
-          icon: const Icon(
-            Icons.menu,
-            color: Color(0xFF333333),
-            size: 25,
-          ),
-          onPressed: () => Scaffold.of(context).openDrawer(),
-        ),
-      ),
+      titleSpacing: 0,
+
+      //TODO: logica para aparecer, carrinho de compras | seta | drawer
+      leading: iconeMenu
+          ? Builder(
+              builder: (context) => IconButton(
+                icon: const Icon(Icons.menu, color: Color(0xFF333333)),
+                onPressed: () => Scaffold.of(context).openDrawer(),
+              ),
+            )
+          : iconeSeta
+          ? IconButton(
+              icon: Icon(Icons.arrow_back, color: Color(0xFF333333)),
+              onPressed: () => Navigator.pop(context),
+            )
+          : null,
+
+      // texto e icone appbar
       title: Row(
         children: [
-          const Icon(
-            Icons.book_outlined,
-            color: Color(0xFF4A7FA5),
-            size: 25,
-          ),
-          const SizedBox(width: 8),
+          Icon(Icons.book_outlined, color: corDoTexto, size: 25),
+          SizedBox(width: 8),
           Text(
             title,
-            style: const TextStyle(
-              color: Color(0xFF4A7FA5),
+            style: TextStyle(
+              color: corDoTexto,
               fontWeight: FontWeight.w700,
               fontSize: 21,
             ),
           ),
         ],
       ),
-      bottom: PreferredSize(
-        preferredSize: const Size.fromHeight(1.0),
-        child: Container(
-          color: Colors.black12,
-          height: 1.0,
-        ),
-      ),
+
+      actions: [
+        if (iconeCarrinho)
+          IconButton(
+            icon: Icon(
+              Icons.shopping_cart_outlined,
+              color: Color(0xFF333333),
+            ),
+            onPressed: () {},
+          ),
+      ],
     );
   }
 }
