b2ea01b69d66fd9524e67ebf15813f32713b7c66
PedroViniciusMD <pedrodelgado583@gmail.com>
2026-05-24 13:36:02 -0300

    feat: config de url

---
+++ lib/config/url_config.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/foundation.dart';
+import 'dart:io';
+
+class ApiConfig {
+  static String get baseUrl {
+    if (kIsWeb) {
+      return 'http://localhost:8000';
+    }
+
+    if (Platform.isAndroid) {
+      return 'http://10.0.2.2:8000';
+    }
+
+    if (Platform.isIOS) {
+      return 'http://localhost:8000';
+    }
+
+    return 'http://localhost:8000';
+  }
+}
